705cd805d574115d95ab172f68f3ecb8ff367446
zubair farooq <zubair.farooq@hotmail.com>
2019-07-28 12:44:48 +0500

    Crud Operations and Ajax Create Operation Working

---
+++ DealDouble.Web/Controllers/AuctionsController.cs
@@ -0,0 +1,73 @@
+﻿using DealDouble.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DealDouble.Services;
+
+namespace DealDouble.Web.Controllers
+{
+    public class AuctionsController : Controller
+    {
+        
+        // List Auction
+        [HttpGet]
+        public ActionResult Index()
+        {
+            AuctionService service = new AuctionService();
+            var auctions = service.GetAllAuction();
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView(auctions);
+            }
+            return View(auctions);
+        }
+        // Create Auction
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Auction auction)
+        {
+            AuctionService service = new AuctionService();
+            service.AddAuction(auction);
+            return RedirectToAction("Index");
+        }
+        // Edit Auction
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            AuctionService service = new AuctionService();
+            var auction = service.GetAuctionByID(ID);
+            return View(auction);
+        }
+        
+        [HttpPost]
+        public ActionResult Edit(Auction auction)
+        {
+            AuctionService service = new AuctionService();
+            service.UpdateAuction(auction);
+            return RedirectToAction("Index");
+        }
+      
+        // Delete Auction
+        [HttpGet]
+        public ActionResult Delete(int ID)
+        {
+            AuctionService service = new AuctionService();
+            var auction = service.GetAuctionByID(ID);
+            return View(auction);
+        }
+        [HttpPost]
+        public ActionResult Delete(Auction auction)
+        {
+            AuctionService service = new AuctionService();
+            service.DeleteAuction(auction);
+            return RedirectToAction("Index");
+        }
+    }
+}
